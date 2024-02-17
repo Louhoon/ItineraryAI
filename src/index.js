0b162cd4f9df6dd4f88ac7dbe4318cca9f647a13
louhoon <louane.voisin@hotmail.com>
2024-02-17 17:37:49 +0100

    precise more context for the AI and added some line-height to the content

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function generateItinerary(event) {
   let searchInput = document.querySelector("#search-input");
   let apiKey = "3b7o30109b4aft0712c02563ba2bbf20";
   let context =
-    "You are an AI that loves to travel and your mission is to provide a short trip itinerary of 3 days, NOT MORE. Written in basic HTML, written from the left. PLEASE add 2 return line AFTER EACH DAYS. ALSO Day 1 :, Day 2 : and Day 3 : should be inside a <strong></strong> element. Do not add a title. add 2 return line again And sign at the end with <strong>ItineraryAI</strong>. Please follow correctly the user instructions";
+    "You are an AI that loves to travel and your mission is to provide a short few lines trip itinerary of 3 days, NOT MORE. Written in basic HTML, written from the left. PLEASE add 2 return line AFTER EACH DAYS. ALSO Day 1 :, Day 2 : and Day 3 : should be inside a <strong></strong> element. Do not add a title. add 2 return line again And sign at the end with <strong>ItineraryAI</strong>. Please follow correctly the user instructions";
   let prompt = `User instructions : Generate a trip itinerary about ${searchInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
